4596d2e63c0ba705846ab27642dad0bc2910f948
TUSHARSAHU15 <tusharsahu1511@gmail.com>
2026-08-16 21:54:35 +0530

    Bind Gunicorn to 0.0.0.0:\ in Procfile, app.py, and wsgi.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:application
+web: gunicorn app:app --bind 0.0.0.0:$PORT
--- app.py
+++ app.py
@@ -337,4 +337,5 @@ def api_export_csv():
 application = app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
